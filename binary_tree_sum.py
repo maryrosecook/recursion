3a4119829e213705657bb0a2b68b18b0ef979860
Mary Rose Cook <maryrosecook@maryrosecook.com>
2014-12-10 18:18:39 -0500

    Add binary tree sum

---
+++ binary_tree_sum.py
@@ -0,0 +1,23 @@
+class Node(object):
+    def __init__(self, value, l=None, r=None):
+        self.value = value
+        self.left = l
+        self.right = r
+
+    def sum(self):
+        return self.value + \
+            (self.left and self.left.sum() or 0) + \
+            (self.right and self.right.sum() or 0)
+
+print str(Node(1,
+               Node(2,
+                    Node(3),
+                    Node(4)),
+               Node(5,
+                    Node(6),
+                    Node(7))).sum()) + " == 28"
+
+print str(Node(1,
+               Node(2,
+                    Node(3)),
+               Node(4)).sum()) + " == 10"
