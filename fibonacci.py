021ef4d09add33401b5fe9ed120f6814d3d62cfa
Mary Rose Cook <maryrosecook@maryrosecook.com>
2014-12-10 10:10:42 -0500

    fib

---
+++ fibonacci.py
@@ -0,0 +1,12 @@
+def fib(n):
+    if n == 1 or n == 2:
+        return 1
+    else:
+        return fib(n - 1) + fib(n - 2)
+
+print fib(1)
+print fib(2)
+print fib(3)
+print fib(4)
+print fib(5)
+print fib(6)
